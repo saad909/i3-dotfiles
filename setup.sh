92d4ba057bcb2eb30206149ee4213a66f5042bfc
saad <abuhurraira.saad909@gmail.com>
2021-04-05 22:46:10 +0500

    made script modular and installing packages from reading a file

--- setup.sh
+++ setup.sh
@@ -1,87 +1,78 @@
-my_setup(){
+# install paru
+install_paru(){
+	cd /tmp
+	sudo pacman -S git
+	git clone https://aur.archlinux.org/paru.git
+	cd paru; makepkg -si
+}
+install_packages(){
+	# install pacman programs
+	sudo pacman -S - < packages.txt --noconfirm --needed
+}
+install_aur_packages(){
+	# install aur programs
+	paru -S - < aur_packages.txt --noconfirm --needed
+}
+install_dotfiles(){
+	# install dot files
+	cd $HOME/.dotfiles
+	stow home
+}
+set_wallpaper(){
+	# setup wallpaper
+	$HOME/.config/scripts/setbg $HOME/.dotfiles/wallpaper.jpg
+}
+install_bluetooth(){
+	# bluetooth setup
+	sudo cp /etc/bluetooth/main.conf{,.bak}
+	echo "
+	AutoEnable=true
+	" | sudo tee -a /etc/bluetooth/main.conf
+	sudo systemctl start bluetooth.service
+	sudo systemctl enable bluetooth.service
+}
+install_vmware(){
 	# install vmware
-	echo "(7/9)installing vmware"
 	paru -S  vmware-workstation --noconfirm --needed
+}
+install_securecrt(){
 	# install securecrt
-	echo "(8/9)installation securecrt"
 	cd /tmp
 	git clone https://github.com/saad909/securecrt-1 scrt
-	cd scrt; chmod +x ./installation.txt;./installation.txt
-	# install gns3
-	echo "(9/9)installing gns3"
-	sudo pacman -S lib32-openssl lib32-gcc-libs --noconfirm --needed
-	paru -S gns3-gui gns3-server  ubridge --noconfirm --needed
-	#install keepass and add its plugins
-	#install nvidia drivers
-	#setup for my fyp
+	cd scrt; chmod +x ./installation.txt;./installation.txt && cd rm ../scrt -rf && cd $HOME/.dotfiles
 }
-
-# install paru
-echo "(1/9)installing paru"
-cd /tmp
-sudo pacman -S git
-git clone https://aur.archlinux.org/paru.git
-cd paru; makepkg -si
-
-# install pacman programs
-echo "(2/9)installing programs"
-sudo pacman -S base-devel --needed;\
-	sudo pacman -S git glibc rofi ranger alacritty picom dmenu vim xclip sxhkd \
-       thunar pcmanfm python-pip npm ueberzug xwallpaper atool \
-       xcape tmux zsh cmus qutebrowser firefox galculator \
-       youtube-dl arandr xrandr putty zsh-autosuggestions fzf \
-       zsh-syntax-highlighting stow  libvirt gromit-mpx --noconfirm 
-# install aur programs
-echo "(3/9)installing aur programs"
-paru -S neovim-nightly-bin ly-git i3-gaps-rounded-git \
-	autotiling arch-wiki-dmenu polybar aic94xx-firmware \
-	udiskie-dmenu-git networkmanager-dmenu btmenu \
-       	upd72020x-fw  wd719x-firmware --noconfirm
-
-# install dot files
-echo "(4/9)installing dotfiles"
-cd $HOME/.dotfiles
-stow home
-# install gtk theme
-echo "(5/9)installing gtk themes"
-paru -S yaru-icon-theme-git yaru-sound-theme-git yaru-gtk-theme-git\
-	--noconfirm --needed
-
-# setup wallpaper
-echo "(6/9)setting wallpaper"
-$HOME/.config/scripts/setbg $HOME/.dotfiles/wallpaper.jpg
-
-# android setup
-echo "(7/9)android setup"
-puru --needed -S mtpfs jmtpfs gvfs-mtp gvfs-gphoto2  --noconfirm
-
-# bluetooth setup
-echo "(8/9)Bluetooth setup"
-sudo pacman -S bluez bluez-utils-compat blueman --noconfirm
-sudo cp /etc/bluetooth/main.conf{,.bak}
-echo "
-AutoEnable=yes
-" | sudo tee - a/etc/bluetooth/main.conf
-sudo pacman -S pulseaudio pulseaudio-bluetooth pavucontrol --noconfirm
-
-# enabling services at startup
-echo "(9/9)Enabling services"
-sudo systemctl enable ly
-sudo systemctl start bluetooth.service
-sudo systemctl enable bluetooth.service
-
-################### Networking ###############################
-printf "Want to setup for networking?"
-read selection
-if [[ $selection -eq 'y' || $selection -eq 'Y' ]]
-then
-	my_setup
-elif [[ $selection -eq 'n' || $selection -eq 'N' ]]
-then
-	exit 0
-fi
-
+install_gns3(){
+	paru -S gns3-gui gns3-server  ubridge --noconfirm --needed
+}
+setup_for_networking(){
+	# setup for networking
+	printf "Want to setup for networking(y/n)"
+	read selection
+	if [[ $selection == 'y' || $selection == 'Y' ]]
+	then
+		install_vmware()
+		install_securecrt()
+		echo "Done ........"
+		return
+	else
+		echo "Done ........"
+		return
+	fi
+}
+# functions
+printf "\n\n\t\t------------------------ Step(1/6) - Installing paru ------------------------\n\n"
+install_paru
+printf "\n\n\t\t------------------------ Step(2/6) install packages ------------------------\n\n"
+install_packages
+printf "\n\n\t\t------------------------ Step(3/6) installing aur packages ------------------------\n\n"
+install_aur_packages
+printf "\n\n\t\t------------------------ Step(4/6) installing dotfiles ------------------------\n\n"
+install_dotfiles
+printf "\n\n\t\t------------------------ Step(5/6) setting up wallpaper ------------------------\n\n"
+set_wallpaper
+printf "\n\n\t\t------------------------ Step(6/6) setting up bluetooth ------------------------\n\n"
+settingup_bluetooth
+# for networking setup
+printf "\n\n\t\t------------------------ Networking Section ------------------------\n\n"
+setup_for_networking
 # enable service of ly
-# android and bluetooth setup remaining
-echo "Done........"
-
