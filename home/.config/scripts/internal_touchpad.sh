16a1ed0c7a15fe53312e29ad92452c1bfa9187ec
saad <abuhurraira.saad909@gmail.com>
2021-04-29 16:08:05 +0500

    Mouse stick enable/disable also added to internal_touchpad script

--- home/.config/scripts/internal_touchpad.sh
+++ home/.config/scripts/internal_touchpad.sh
@@ -24,3 +24,26 @@ else
 	xinput reattach $id $master_id
 	notify-send "Touchpad is enabled"
 fi
+
+mouse_stick=$( xinput list  | grep -i "stick.*id=" | wc -l )
+id=$(xinput list  | grep -i stick | sed -e s:".*id="::g | cut -c -2)
+echo $id > /tmp/stick_id
+
+# touchpad is enabled
+if [[ $touchpad_running != "1" ]]
+then
+	# get the master id and write  it to the /tmp/master_id
+	id=$(cat /tmp/stick_id)
+	master_id=$(xinput list | grep -i stick | sed -r s:".*\("::g | cut -c -1)
+	echo $master_id > /tmp/stick_masterid
+	xinput float $id
+	notify-send "Mouse stick is disabled"
+	
+	# keyboard is not running
+else
+
+	id=$(cat /tmp/stick_id)
+	master_id=$(cat /tmp/stick_masterid)
+	xinput reattach $id $master_id
+	notify-send "Stick is enabled"
+fi
